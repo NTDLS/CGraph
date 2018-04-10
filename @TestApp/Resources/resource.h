37f4372cd3a20cef3c3cc5f4496451436ea1a7f8
Josh Patterson <josh@ntdls.com>
2018-04-09 21:58:42 -0400

    Add files via upload

---
+++ @TestApp/Resources/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDI_MAINICON                    101
+#define IDI_MAIN                        101
+#define IDD_MAIN                        102
+#define IDC_TEXTBOX                     1000
+#define IDC_OK                          1001
+#define IDC_CANCEL                      1002
+#define IDC_STATICTEXT                  1003
+#define IDC_GLLOGO                      1010
+#define IDC_GRAPH                       1010
+#define IDC_GRAPH1                      1010
+#define IDC_GRAPH2                      1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
